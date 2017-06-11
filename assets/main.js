0e942cbd2e67fcdf4225ff11d12e55870b077e29
Bruce Tsvil <superunknown1103@gmail.com>
2017-06-11 10:16:27 -0500

    it begins

---
+++ assets/main.js
@@ -0,0 +1,10 @@
+var topics = ['Mr. Burns','Smithers','Homer Simpson','Lisa Simpson','Bart Simpson','Moe Syzslak','Krusty the Clown','Lenny Leonard','Carl Carlson','Barney Gumble','Itchy and Scratchy','Cheif Wiggum','Mayor Quimby', 
+'Apu Nahasapeemapetilon','Doctor Hibbert'];
+
+function forloop(){
+    	for (var i = 0; i < topics.length; i++){
+        console.log(topics[i]);
+    };
+};
+
+forloop();
